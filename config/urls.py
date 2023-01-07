656c7c5c7b74e36d5b84405d3cf58fdb21558843
s2alua <s2alua.fu@gmail.com>
2023-01-07 08:57:09 +0000

    merge

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('photo-add/', admin.site.urls),
+    path('common/', include('common.urls')),
     path('', include('cook.urls')),
 ]
